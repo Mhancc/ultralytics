aeb1a0806aa142229758d71c857980414fa7bfff
menghan <menghan@ieslab.com>
2024-06-04 17:11:15 +0800

    fix bug

--- ultralytics/utils/callbacks/aicc.py
+++ ultralytics/utils/callbacks/aicc.py
@@ -71,19 +71,19 @@ def teardown(trainer):
 
 callbacks = (
     {
-        "on_pretrain_routine_start": [on_pretrain_routine_start],
-        "on_pretrain_routine_end": [on_pretrain_routine_end],
-        "on_train_start": [on_train_start],
-        "on_train_epoch_start": [on_train_epoch_start],
-        "on_train_batch_start": [on_train_batch_start],
-        "optimizer_step": [optimizer_step],
-        "on_before_zero_grad": [on_before_zero_grad],
-        "on_train_batch_end": [on_train_batch_end],
-        "on_train_epoch_end": [on_train_epoch_end],
-        "on_fit_epoch_end": [on_fit_epoch_end],  # fit = train + val
-        "on_model_save": [on_model_save],
-        "on_train_end": [on_train_end],
-        "on_params_update": [on_params_update],
-        "teardown": [teardown],
+        "on_pretrain_routine_start": on_pretrain_routine_start,
+        "on_pretrain_routine_end": on_pretrain_routine_end,
+        "on_train_start": on_train_start,
+        "on_train_epoch_start": on_train_epoch_start,
+        "on_train_batch_start": on_train_batch_start,
+        "optimizer_step": optimizer_step,
+        "on_before_zero_grad": on_before_zero_grad,
+        "on_train_batch_end": on_train_batch_end,
+        "on_train_epoch_end": on_train_epoch_end,
+        "on_fit_epoch_end": on_fit_epoch_end,  # fit = train + val
+        "on_model_save": on_model_save,
+        "on_train_end": on_train_end,
+        "on_params_update": on_params_update,
+        "teardown": teardown,
     }
 )
--- ultralytics/utils/callbacks/base.py
+++ ultralytics/utils/callbacks/base.py
@@ -208,8 +208,9 @@ def add_integration_callbacks(instance):
         from .raytune import callbacks as tune_cb
         from .tensorboard import callbacks as tb_cb
         from .wb import callbacks as wb_cb
+        from .aicc import callbacks as aicc_cb
 
-        callbacks_list.extend([clear_cb, comet_cb, dvc_cb, mlflow_cb, neptune_cb, tune_cb, tb_cb, wb_cb])
+        callbacks_list.extend([clear_cb, comet_cb, dvc_cb, mlflow_cb, neptune_cb, tune_cb, tb_cb, wb_cb, aicc_cb])
 
     # Add the callbacks to the callbacks dictionary
     for callbacks in callbacks_list:
